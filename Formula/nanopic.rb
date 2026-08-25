2b803da97f09b56b260f138ac057b40ff64fddeb
wyh <wangyuehuan@yeah.net>
2026-08-25 09:10:35 +0800

    Add 1.0.0 all-arch bottle (pour without Command Line Tools)

--- Formula/nanopic.rb
+++ Formula/nanopic.rb
@@ -5,6 +5,11 @@ class Nanopic < Formula
   url "https://github.com/wangyuehuan-git/NanoPic/releases/download/v1.0.0/nanopic-1.0.0-universal.tar.gz"
   sha256 "f0f947301b89945135ae807fd88912c4a4b1be512940a5ea55afba407207a129"
 
+  bottle do
+    root_url "https://github.com/wangyuehuan-git/NanoPic/releases/download/v1.0.0"
+    sha256 cellar: :any_skip_relocation, all: "2de7ac3010bd66856974633227fed14d98a1917a394fda578aca45cdcd46f0b9"
+  end
+
   def install
     bin.install "nanopic"
   end
